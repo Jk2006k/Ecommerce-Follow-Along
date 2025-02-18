87a0f5446873f3c76893546756dcf4ea3dc8689e
jk2006k <jk05kishoore@gmail.com>
2025-02-18 15:16:46 +0530

    milestone-25

--- Backend/routes/Router.js
+++ Backend/routes/Router.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const{login,signUp,getUserProfile, addAddress, removeAddress, getAllAddresses}=require('../controllers/UserControllers');
 const { upload } = require('../config/multer');
 const authMiddleware = require('../middleware/authmiddlware');
+const {createOrder}=require('../controllers/OrderControllers')
 
 
 router.post('/signup',upload.single('file'),signUp);
@@ -11,5 +12,6 @@ router.post('/profile',getUserProfile)
 router.post('/profile/address',addAddress)
 router.post('/profile/remove',removeAddress)
 router.post('/profile/addresses',getAllAddresses)
+router.post('/orders', createOrder); 
 
 module.exports=router;
